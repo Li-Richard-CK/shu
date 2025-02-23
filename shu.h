93a6e4f08ad72852d61056a0db68d69f473adbac
Li-Richard <lirichardck@gmail.com>
2025-02-23 08:00:46 +0800

    added shu.h and updated README.md

---
+++ shu.h
@@ -0,0 +1,23 @@
+/* shu.h - public domain single-header-file utilities - Li Richard 2025
+ *
+ * This is a collection of all shu libraries.
+ * Will not define no short names or implementation by default.
+ *
+ * Documentation:
+ *    man 3 shu
+*/
+#ifndef LIBSHU_H
+#define LIBSHU_H
+
+#ifdef SHU_IMPLEMENTATION
+#define SHU_DS_IMPLEMENTATION
+#endif
+
+#ifdef SHU_NO_SHORT_NAMES
+#define SHU_DS_NO_SHORT_NAMES
+#endif
+
+#include "shu_ds.h"
+
+#endif
+
